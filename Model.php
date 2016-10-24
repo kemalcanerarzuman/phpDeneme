6c2aa0c0f8809c0797aedfbcdab2d7cad4ae07f5
KCanerArzuman <kemalcanerarzuman@gmail.com>
2016-10-24 17:58:54 +0300

    24.10.16 18:00

---
+++ Model.php
@@ -0,0 +1,89 @@
+<?php
+
+class Model {
+
+  protected $fileName = null;
+  protected $columns = array();
+
+  private $data = null;
+
+  private function getHandle($mode = "r"){
+    return fopen($this->fileName, $mode);
+  }
+
+  public function getLines() {
+    if (!$this->data) {
+      $fileSize = filesize($this->fileName);
+      $handle = $this->getHandle();
+      $this->data = $fileSize > 0 ? trim(fread($handle, $fileSize)) : "";
+      fclose($handle);
+    }
+    return trim($this->data) != "" ? explode("\n", $this->data) : [];
+  }
+
+  public function indexOf($column, $value) {
+    foreach ($this->getRows() as $key => $line) {
+      if (isset($line[$column]) && $line[$column] === $value) return $key;
+    }
+    return -1;
+  }
+
+  public function find($column=null, $value=null) {
+    if($column || $value){
+      return $this->getRows();
+    } else {
+      foreach ($this->getLines() as $key => $value1) {
+        if($column == $key && $value == $value1) {
+          return $this->getRows()[$key];
+        }
+      }
+    }
+    return null;
+  }
+  public function create($array) {
+    $toAppend;
+    if(count($array) == 3) {
+      fwrite(  $this->getHandle("a"), implode(',',$array)."\n");
+    }
+  }
+
+  public function count() {
+    return count($this->getLines());
+  }
+
+  public function __call($name, $args = []) {
+/*    if (strpos($name, "findBy") === 0) {
+      $column = strtolower(str_replace("findBy", "", $name));
+      if (!in_array($column, $this->columns)) throw new Exception("Column $column not found");
+      return $this->find($column, $args[0]);
+    }
+    throw new Exception("Method $name not found");
+*/
+  }
+
+  public function getRows() {
+    $returnVal = [];
+    foreach ($this->getLines() as $lineStr) {
+      $lineData = explode(",", $lineStr);
+      $row = [];
+      foreach ($this->columns as $colIndex => $colName) {
+        $row[$colName] = $lineData[$colIndex];
+      }
+      $returnVal[] = $row;
+    }
+    return $returnVal;
+  }
+
+  private function setColumnsFromFile(){
+    $arr = file("$this->fileName");
+    $columns;
+    for($i = 0; $i < count($arr); $i++){
+        $columnValues = explode(",",$arr[$i]);
+        $columns[] = $columnValues;
+  }
+    return $columns;
+  }
+
+
+}
+?>
--- yazarlar.php
+++ yazarlar.php
@@ -1,16 +1,37 @@
-  <?php
+<?php
 
-    require_once ("function/functions.php");
-    startHTML();
-    $yazarlar = readTxtFile("yazarlar.txt");
-    if($_POST){
-      $_POST = readPOST($_POST,"yazarlar.txt",$yazarlar);
-    }
+  require_once "Yazar.php";
 
-    $yazarlar = readTxtFile("yazarlar.txt");
-    createTable($yazarlar);
+  $yazar = new Yazar();
 
-    createForm();
+  echo "<pre>";
+  //print_r($yazar->findByName("ss"));
 
-    finishHTML();
-  ?>
+  $yazar->create(array(
+    'name' => 'asdd',
+    'surname' => 'dddd',
+    'birth_year' => 1995
+  ));
+  print_r($yazar->findByName("asdd"));
+  echo $yazar->count();
+
+  echo  $yazar->find("name","ss");
+
+  echo "</pre>";
+
+/*
+  require_once ("function/functions.php");
+  startHTML();
+  $yazarlar = readTxtFile("yazarlar.txt");
+  if($_POST){
+    $_POST = readPOST($_POST,"yazarlar.txt",$yazarlar);
+  }
+
+  $yazarlar = readTxtFile("yazarlar.txt");
+  createTable($yazarlar);
+
+  createForm();
+
+  finishHTML();
+  */
+?>
